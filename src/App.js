817eedacb809e5d03419f88bf20b51a0fe87fdf8
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-05-16 19:09:24 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const numbers = [23, 61, 31, 55, 78];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {numbers.map((number) => {
+        return (
+          <span>
+            {number + 3}
+            <br />
+          </span>
+        );
+      })}
     </div>
   );
 }
